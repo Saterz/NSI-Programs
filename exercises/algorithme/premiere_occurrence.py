dd7e7d346314818bb76eb11fa759f2ef68e8aede
Saterz_ <git@saterz.dev>
2026-03-26 08:43:34 +0100

    Fix premiere_occurrence.py

--- exercises/algorithme/premiere_occurrence.py
+++ exercises/algorithme/premiere_occurrence.py
@@ -1,10 +1,10 @@
-def premi_occu(liste: list[int], elem: int)-> int | None: # 3n + 6 = O(n)
+def premi_occu(liste: list[int], elem: int)-> int | None: # 3n + 5 = O(n)
     n: int = len(liste) # 1
     position: int | None = None # 1
     i = 0 # 1
     while i < n and position == None: # n + 1
         if liste[i] == elem: # n
-            position = i # 1
+            position = i # 0
         else:
             i += 1 # n
     return position # 1
